9d00dbfa75a8c969b2cb8b24fcd12340e087817f
GurjashanSinghChugh6862 <gurjashansinghchugh@gmail.com>
2026-09-14 15:19:07 +0000

    first hour revision

---
+++ 01_basics/index.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "gurjashansinghchugh@gmail.com";
+var accountPassword = "asdfghj";
+accountCity = "Abohar";
+
+accountEmail = "gurjashan@gmail.com"
+accountPassword = "Pass@123"
+accountCity = "Bathinda"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 03_conversion/index.js
@@ -0,0 +1,46 @@
+// let score = null
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+/*
+
+"33" -> 33
+"33adad" -> NAN
+true -> 1
+false -> 0
+undefined -> NAN
+null -> 0
+
+*/
+
+// let isLoggedIn = undefined
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+// let num = 33
+// let stringNum = String(num)
+// console.log(typeof stringNum)
+// console.log(stringNum)
+
+
+
+// ************* Operations ****************************
+
+let val = 3
+let negVal = -val
+console.log(negVal)
+
+
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
